f085815ef2a93a99a2cb1dbce9051fc00031f563
Akash Khandelwal <er.akashkhandelwal@gmail.com>
2013-08-27 19:56:34 +0530

    Implement follow functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :user_name
 
 	has_many :tweets, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+	has_many :followers, through: :reverse_relationships
 
 	validates :name, presence: true
 	validates :user_name, presence: true, uniqueness: true
