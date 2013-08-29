69ed531dbfb43e6364f73dfd6fdf347e6e8d1f54
Akash Khandelwal <er.akashkhandelwal@gmail.com>
2013-08-29 12:13:34 +0530

    add following_user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+
+  def create
+  	user = User.new(params[:user])
+  	if user.save
+  		sign_in user
+  		redirect_to user_path(user), notice: "Signed up!"
+  	else
+  		render "new"
+  	end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def follow_user
+    debugger
   end
 end
