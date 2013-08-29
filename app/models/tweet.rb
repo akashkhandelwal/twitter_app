ac70b46f9d56fed5703d8f481020654209862baa
Akash Khandelwal <er.akashkhandelwal@gmail.com>
2013-08-29 18:44:28 +0530

    Relationship between tweets and users

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,20 +4,26 @@
 #
 #  id         :integer          not null, primary key
 #  body       :string(255)
-#  user_id    :integer
 #  created_at :datetime
 #  updated_at :datetime
 #  private    :boolean
 #
 
 class Tweet < ActiveRecord::Base
-	attr_accessible :body, :user_id, :private
+	attr_accessible :body, :private
 
-	belongs_to :user
+	has_many :users, through: :tweet_users
+	has_many :tweet_users
+
 	validates :body, presence: true, length: { maximum: 160 }
-	validates :user_id, presence: true
 
 	def set_tweet_as_private 
 		self.private = true
+	end
+
+	def associate_tweet_with_user user
+		relationship = self.tweet_users.build
+		relationship.user = user
+		relationship.save
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@
 #
 #  id            :integer          not null, primary key
 #  name          :string(255)
-#  email         :string(255)
+#  username      :string(255)
 #  password_hash :string(255)
 #  password_salt :string(255)
 #  created_at    :datetime
@@ -12,24 +12,25 @@
 #
 
 class User < ActiveRecord::Base
-	attr_accessible :name, :email, :password, :password_confirmation
+	attr_accessible :name, :username, :password, :password_confirmation
 	attr_accessor :password
 
 	before_save :encrypt_password
 
-	has_many :tweets, dependent: :destroy
+	has_many :tweets, through: :tweet_users
+	has_many :tweet_users, dependent: :destroy
 	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 	has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
 	has_many :followed_users, through: :relationships, source: :followed
 	has_many :followers, through: :reverse_relationships
 
 	validates_presence_of :name
-	validates :email, presence: true, uniqueness: true
+	validates :username, presence: true, uniqueness: true
 	validates_presence_of :password, on: :create
 	validates_confirmation_of :password
 
-	def self.authenticate(email, password)
-    user = find_by_email(email)
+	def self.authenticate(username, password)
+    user = find_by_username(username)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
@@ -42,5 +43,17 @@ class User < ActiveRecord::Base
       self.password_salt = BCrypt::Engine.generate_salt
       self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
     end
+  end
+
+  def following? user
+  	relationships.find_by_followed_id(user.id)
+  end
+
+  def retweeted? tweet
+  	tweet_users.find_by_tweet_id(tweet.followed_id)
+  end
+
+  def to_param
+  	username
   end
 end
