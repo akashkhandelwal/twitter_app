7fd2232aec488bd9ed74b77fd0a11639c4dc8c96
Akash Khandelwal <er.akashkhandelwal@gmail.com>
2013-08-29 12:05:35 +0530

    Changes in database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130827134041) do
+ActiveRecord::Schema.define(version: 20130828193911) do
 
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
@@ -25,11 +25,14 @@ ActiveRecord::Schema.define(version: 20130827134041) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "private"
   end
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "user_name"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
